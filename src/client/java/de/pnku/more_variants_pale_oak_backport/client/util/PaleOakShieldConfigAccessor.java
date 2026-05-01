df2c887ab23965cb8e7eba90766f2c274723fdc2
pnku <pnku@pnku.de>
2026-05-01 14:43:31 +0200

    Remove Mixins extracted to MV Core

--- src/client/java/de/pnku/more_variants_pale_oak_backport/client/util/PaleOakShieldConfigAccessor.java
+++
@@ -1,8 +0,0 @@
-package de.pnku.more_variants_pale_oak_backport.client.util;
-
-public interface PaleOakShieldConfigAccessor {
-    boolean mvpob$isPaleOakUseCustom();
-
-    void mvpob$setPaleOakUseCustom(boolean useCustom);
-}
-
--- src/main/java/de/pnku/more_variants_pale_oak_backport/util/Type.java
+++
@@ -1,5 +0,0 @@
-package de.pnku.more_variants_pale_oak_backport.util;
-
-public interface Type {
-    String registrationType();
-}
