57e3df265ad6088412e99abe28018805c0990664
pnku <pnku@pnku.de>
2026-05-27 22:44:57 +0200

    Remove Planks Id in Wood definition (`1.0.0-d:c2`)

--- src/main/java/de/pnku/more_variants_pale_oak_backport/WoodTypes.java
+++ src/main/java/de/pnku/more_variants_pale_oak_backport/WoodTypes.java
@@ -5,5 +5,5 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.material.MapColor;
 
 public class WoodTypes {
-    public static final MoreVariantWoodType PALE_OAK = new MoreVariantWoodType("pale_oak", MapColor.QUARTZ, ResourceLocation.withDefaultNamespace("pale_oak_planks"), 13);
+    public static final MoreVariantWoodType PALE_OAK = new MoreVariantWoodType("pale_oak", MapColor.QUARTZ, 13);
 }
